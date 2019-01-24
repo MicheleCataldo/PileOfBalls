e9ba93760d36fecbd5cb0f7c1b3794b9e810e49e
MicheleCataldo <michelecataldo1994@gmail.com>
2019-01-24 13:50:15 +0100

    aggiunta classe Pair

---
+++ PileOfBalls/src/esami/ia/pileofballs/Pair.java
@@ -0,0 +1,27 @@
+package esami.ia.pileofballs;
+
+public class Pair {
+	private int i;
+	private int j;
+	
+	public Pair(int i, int j){
+		this.setI(i);
+		this.setJ(j);
+	}
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+	public void setJ(int j) {
+		this.j = j;
+	}
+}
