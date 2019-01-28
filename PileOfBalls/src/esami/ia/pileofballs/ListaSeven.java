e0f36c102332cabe890f720028269a46d4e5dc4b
MicheleCataldo <michelecataldo1994@gmail.com>
2019-01-28 14:23:37 +0100

    tolta ereditarietà

--- PileOfBalls/src/esami/ia/pileofballs/ListaSeven.java
+++ PileOfBalls/src/esami/ia/pileofballs/ListaSeven.java
@@ -2,10 +2,46 @@ package esami.ia.pileofballs;
 
 import java.awt.Color;
 
-public class ListaSeven extends Lista {
+public class ListaSeven {
+	private int first;
+	private int second;
+	private int third;
+	private int four;
 	private int five;
 	private int six;
 	private int seven;
+	
+	public int getFirst() {
+		return first;
+	}
+
+	public void setFirst(int first) {
+		this.first = first;
+	}
+
+	public int getSecond() {
+		return second;
+	}
+
+	public void setSecond(int second) {
+		this.second = second;
+	}
+
+	public int getThird() {
+		return third;
+	}
+
+	public void setThird(int third) {
+		this.third = third;
+	}
+
+	public int getFour() {
+		return four;
+	}
+
+	public void setFour(int four) {
+		this.four = four;
+	}
 
 	public int getFive() {
 		return five;
@@ -31,22 +67,45 @@ public class ListaSeven extends Lista {
 		this.seven = seven;
 	}
 
-	public ListaSeven(int first, int second, int third, Color color, int five, int six, int seven) {
-		super(first, second, third, color);
-		this.setFive(five);
-		this.setSix(six);
-		this.setSeven(seven);
-	}
-
 	public ListaSeven(int first, int second, int third, int four, int five, int six, int seven) {
-		super(first, second, third, four);
+		this.setFirst(first);
+		this.setSecond(second);
+		this.setThird(third);
+		this.setFour(four);
 		this.setFive(five);
 		this.setSix(six);
 		this.setSeven(seven);
 	}
-
-	public ListaSeven() {
-		// TODO Auto-generated constructor stub
+	
+	public ListaSeven(){
+		this.first = 0;
+		this.second = 0;
+		this.third = 0;
+		this.four = 0;
+		this.five = 0;
+		this.six = 0;
+		this.seven = 0;
+	}
+	
+	public String toString(){
+		return  "("+this.first+", "+this.second+", "+this.third+", "+this.four+", "+this.five+", "+this.six+", "+this.seven+")";
+	}
+	
+	public Color getFourColor(int c){
+		switch(c){
+			case 0:
+				return Color.BLUE;
+			case 1:
+				return Color.GREEN;
+			case 2: 
+				return Color.RED;
+			case 3:
+				return Color.ORANGE;
+			case 4:
+				return Color.MAGENTA;
+			default:
+				return null;
+		}
 	}
 
 }
