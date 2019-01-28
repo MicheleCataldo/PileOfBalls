a3411dddfd5bb4c5c4c60a49789dcbfce1e5ab3e
MicheleCataldo <michelecataldo1994@gmail.com>
2019-01-28 10:12:47 +0100

    aggiunte posizioni libere all'ia

--- PileOfBalls/src/esami/ia/dlv/IA.java
+++ PileOfBalls/src/esami/ia/dlv/IA.java
@@ -7,7 +7,7 @@ public class IA {
 	
 	public IA(){}
 	
-	public java.util.ArrayList<esami.ia.pileofballs.Lista> scegliPosizione(java.util.ArrayList<esami.ia.pileofballs.Lista> occupati, java.util.ArrayList<esami.ia.pileofballs.Lista> tripla, java.util.ArrayList<esami.ia.pileofballs.Lista> triplasucc, java.util.ArrayList<esami.ia.pileofballs.Pair> costo){
+	public java.util.ArrayList<esami.ia.pileofballs.Lista> scegliPosizione(java.util.ArrayList<esami.ia.pileofballs.Lista> occupati, java.util.ArrayList<esami.ia.pileofballs.Lista> tripla, java.util.ArrayList<esami.ia.pileofballs.Lista> triplasucc, java.util.ArrayList<esami.ia.pileofballs.Pair> costo, java.util.ArrayList<esami.ia.pileofballs.Tris> libere){
 		
 		this.risultato = new java.util.ArrayList<esami.ia.pileofballs.Lista>();
 		this.vuoto = new java.util.ArrayList<esami.ia.pileofballs.Lista>();
@@ -30,6 +30,8 @@ public class IA {
 		it.unical.mat.jdlv.program.JDLV_Logger.getInstance().logInfoMessage("Add in-mapping 'triplasucc::elsucc' in module EXECUTING:"+ it.unical.mat.jdlv.program.JDLV_Logger.getInstance().getPrettyCode(it.unical.mat.jdlv.program.TypeSolver.getNameTranslation(num,"num"), 0));
 		_JDLV_PROGRAM_EXECUTING.addText(it.unical.mat.jdlv.program.TypeSolver.getNameTranslation(costo,"costop"));
 		it.unical.mat.jdlv.program.JDLV_Logger.getInstance().logInfoMessage("Add in-mapping 'costo::costop' in module EXECUTING:"+ it.unical.mat.jdlv.program.JDLV_Logger.getInstance().getPrettyCode(it.unical.mat.jdlv.program.TypeSolver.getNameTranslation(num,"num"), 0));
+		_JDLV_PROGRAM_EXECUTING.addText(it.unical.mat.jdlv.program.TypeSolver.getNameTranslation(libere,"poslibere"));
+		it.unical.mat.jdlv.program.JDLV_Logger.getInstance().logInfoMessage("Add in-mapping 'libere::poslibere' in module EXECUTING:"+ it.unical.mat.jdlv.program.JDLV_Logger.getInstance().getPrettyCode(it.unical.mat.jdlv.program.TypeSolver.getNameTranslation(num,"num"), 0));
 
 		// ---- END - addInMapping ---- 
 		
@@ -82,6 +84,8 @@ public class IA {
 			_JDLV_PROGRAM_EXECUTING.addText(":~ el(ID1,_,_,COL1), elsucc(ID2,_,_,COL2), COL1<>COL2. [1:2]"+'\n');
 			_JDLV_PROGRAM_EXECUTING.addText(":~ costop(ID, C), scelta(ID,_,_,_). [C:3]"+'\n');
 			_JDLV_PROGRAM_EXECUTING.addText(":~ scelta(_,I,_,_), X=11-I. [X:3]"+'\n');
+			_JDLV_PROGRAM_EXECUTING.addText("c(COL) :- scelta(_,_,_,COL), el(_,_,_,COL)."+'\n');
+			
 			_JDLV_PROGRAM_BUFFER_EXECUTING.append("");
 			_JDLV_INVOCATION_EXECUTING.setInputProgram(_JDLV_PROGRAM_EXECUTING);
 			it.unical.mat.wrapper.ModelBufferedHandler _BUFFERED_HANDLER_EXECUTING=new it.unical.mat.wrapper.ModelBufferedHandler(_JDLV_INVOCATION_EXECUTING);
